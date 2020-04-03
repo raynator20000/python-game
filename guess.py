81d63ddbddd5caf48a98e1aee19ea313bc2afa1e
Raymond Li <raynator20000@linux-4nzw.home>
2020-04-03 21:03:03 +0100

    completed chapter 3

--- guess.py
+++ guess.py
@@ -3,30 +3,36 @@ import random
 
 guessesTaken = 0
 
-print('Hello! What is your name?')
+print("Hello! What is your name?")
 myName = input()
 
 number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print("Well, " + myName + ", I am thinking of a number between 1 and 20.")
 
 for guessesTaken in range(6):
-    print('Take a guess.')
+    print("Take a guess.")
     guess = input()
     guess = int(guess)
 
     if guess < number:
-        print('Your guess is too low.')
+        print("Your guess is too low.")
 
     if guess > number:
-        print('Your guess is too high')
+        print("Your guess is too high")
 
     if guess == number:
         break
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    print(
+        "Good job, "
+        + myName
+        + "! You guessed my number in "
+        + guessesTaken
+        + " guesses!"
+    )
 
 if guess != number:
     number = str(number)
-    print('Nope. The number I was thinking of was ' + number + '.')
+    print("Nope. The number I was thinking of was " + number + ".")
